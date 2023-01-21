044279d1aeff9c5fe18134a117152e2ef6ba7951
Kewon <48837736+kewon96@users.noreply.github.com>
2023-01-21 20:17:59 +0900

    Initial commit

---
+++ src/main/java/com/hello/exception/ExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.hello.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc2ExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc2ExceptionApplication.class, args);
+    }
+
+}
