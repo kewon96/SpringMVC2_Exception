6c020ee088d2b97c6f2d961e33741ceb3cc68561
Kewon <48837736+kewon96@users.noreply.github.com>
2023-01-24 01:17:15 +0900

    Error Page

---
+++ src/main/java/com/hello/exception/WebServerCustomizer.java
@@ -0,0 +1,26 @@
+package com.hello.exception;
+
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * 의미가 있기보단 Servlet Container에서 제공
+ * 말 그대로 WebServer(Tomcat)을 Custimizing한다는 것
+ */
+@Component
+public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
+
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
+
+    }
+}
