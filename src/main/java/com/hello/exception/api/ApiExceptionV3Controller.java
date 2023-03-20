70754988f16194173b8f05b4a9815b9330d19022
Kewon <yang0184@naver.com>
2023-03-21 00:06:36 +0900

    @ControllerAdvice

--- src/main/java/com/hello/exception/api/ApiExceptionV3Controller.java
+++ src/main/java/com/hello/exception/api/ApiExceptionV3Controller.java
@@ -1,19 +1,18 @@
 package com.hello.exception.api;
 
-import com.hello.exception.common.ErrorResult;
-import com.hello.exception.exception.UserException;
 import com.hello.exception.exception.UserExceptionChildException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/api2")
-public class ApiExceptionV2Controller {
+@RequestMapping("/api3")
+public class ApiExceptionV3Controller {
 
     @GetMapping("/member/{id}")
     public MemberDto getMember(@PathVariable("id") String id) throws Exception {
